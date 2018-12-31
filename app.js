4c0cb90e3629da84fbb6742968480cfbadeaa4fc
wallace7souza <wallace.c.souza@gmail.com>
2018-12-31 11:38:47 -0300

    Melhorias de anotação  de ações e adicionado agendamento de execução de função

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-global.NODE_ENV = process.env.NODE_ENV || 'development';
+let PROCESS_ENV = null;
+var path = require("path");
+var resolve = path.resolve;
+var fs = require('fs');
+
+process.argv.forEach(function (val, index, array) {
+    if (val.indexOf('NODE_ENV=') > -1) {
+        let processenv = val.replace('NODE_ENV=','');
+        PROCESS_ENV = processenv;
+    }
+});
+
+global.NODE_ENV = PROCESS_ENV || process.env.NODE_ENV || 'development';
+
+
+let path1 = resolve('./config/env/' + NODE_ENV + '.js');
+if (!fs.existsSync(path1)) {
+
+    console.log('Arquivo de ambiente não encontrado ' + path1);
+    process.exit();
+}
+
 var express = require('express');
 var glob = require('glob');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var cors = require('cors')
+var cors = require('cors');
+var forceSsl = require('force-ssl');
 
 var passport = require('passport');
 var passportJWT = require('passport-jwt');
@@ -11,10 +33,9 @@ var ExtractJwt = passportJWT.ExtractJwt;
 var JwtStrategy = passportJWT.Strategy;
 var config = require('./config/env/' + NODE_ENV + '.js');
 
-const path = require('path');
 var annotations = require('annotations');
 var db = require('./config/sequelize');
-
+var CronJob = require('cron').CronJob;
 
 var params = {
     secretOrKey: config.jwtSecret,
@@ -38,6 +59,10 @@ passport.use(strategy);
 
 var app = express();
 app.use(cors());
+forceSsl.https_port = config.app.port;
+if (process.env.NODE_ENV && process.env.NODE_ENV === 'production') {
+    app.use(forceSsl);
+}
 
 function startup() {
 
@@ -46,6 +71,10 @@ function startup() {
     app.use(passport.initialize());
     app.use(bodyParser.urlencoded({extended: false}));
     app.use(cookieParser());
+
+    app.route('/').get(function (req, res, next) {
+        res.send({enviroment: NODE_ENV});
+    });
 
     glob("./controllers/*.controller.js", {}, function (er, files) {
         // files is an array of filenames.
@@ -112,19 +141,19 @@ function startup() {
                             var model = this.trim();
                             if (isJson(model)) {
                                 /** TODO: Fazer validação a partir de json*/
-                            }else{
+                            } else {
                                 /** TODO: Melhorar validação para N níveis  do json*/
                                 var attrs = model.split(',');
-                                attrs.forEach(attr=>{
-                                    if(!req.body.hasOwnProperty(attr)){
-                                    // if(!req.body[attr]){
+                                attrs.forEach(attr => {
+                                    if (!req.body.hasOwnProperty(attr)) {
+                                        // if(!req.body[attr]){
                                         res.status(400).send(`${attr} not found`);
                                         return;
                                     }
-                                })
+                                });
 
                                 next();
-                                
+
                             }
 
                         }.bind(routerAnotations[functionName].model));
@@ -133,15 +162,16 @@ function startup() {
                     chainResponse.push(controller[functionName]);
 
 
-                    var routeMethod = null
+                    var routeMethod = null;
 
-                    if(routerAnotations[functionName].path){
+                    if (routerAnotations[functionName].path) {
                         routeMethod = '/api' + (controller.prefix ? '/' + controller.prefix : '') + routerAnotations[functionName].path;
-                    }else{
+                    } else {
                         routeMethod = '/api' + (controller.prefix ? '/' + controller.prefix : '') + routerAnotations[functionName][requestMethod.toUpperCase()];
                     }
 
                     console.log(requestMethod, routeMethod);
+                    app.route(routeMethod)[requestMethod](chainResponse);
                 }
                 else if (routerAnotations[functionName].PathParam) {
                     app.param(routerAnotations[functionName].PathParam, controller[functionName])
@@ -173,7 +203,26 @@ function startup() {
 
     });
 
-    // require('./cron/send-sms-daily.cron');
+    glob("./service/*.service.js", {}, function (er, files) {
+
+        files.forEach(filePath => {
+            let servicePath = path.resolve(filePath);
+            var service = require(servicePath);
+            var routerAnotations = annotations.getSync(servicePath);
+
+            // console.log(filePath);
+            Object.keys(routerAnotations).forEach(function (functionName) {
+
+                if (routerAnotations[functionName].CronJob) {
+                    let cronJob = new CronJob(routerAnotations[functionName].CronJob, service[functionName], null, true, 'America/Sao_Paulo');
+                }
+                if (routerAnotations[functionName].Init) {
+                    service[functionName]();
+                }
+
+            })
+        })
+    })
 
 
 }
